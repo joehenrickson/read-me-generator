7d35fc21d7916aeb9f9937dc89bd3760d19cc5be
joehenrickson <joehenrickson@gmail.com>
2022-07-22 23:22:15 -0500

    final changes to page-template

--- src/page-template.js
+++ src/page-template.js
@@ -3,7 +3,6 @@ function generateReadme(answers) {
   return `
 <h1 align="center">${answers.projectTitle}</h1>
 
-${answers.license}<br />
 ## Description
 ${answers.description}
 ## Table of Contents
@@ -18,10 +17,7 @@ ${answers.description}
 ${answers.installation}
 ## Usage
 ${answers.usage}
-## License
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
-<br />
-This application is covered by the ${answers.license} license. 
+
 ## Contributing
 ${answers.contributing}
 ## Tests
@@ -32,7 +28,7 @@ ${answers.questions}<br />
 Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
 <br />
 Email me with any questions: ${answers.email}<br /><br />
-_This README was generated with ❤️ by [README-generator](https://github.com/joehenrickson/read-me-generator)
+This README was generated with ❤️ by [README-generator](https://github.com/joehenrickson/read-me-generator)
   `;
 }
 
